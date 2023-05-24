a151e6714ac28823a7d56fc0e18a4ae2c936e814
Jesus Duran <dcardenasj@uninorte.edu.co>
2023-05-24 03:46:26 -0500

    probando sin comando start

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,16 +8,28 @@ app.listen(PORT);
 console.log("server on port ", PORT);
 
 app.get("/", async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM users');
-    res.json(rows)
+  try {
+    const [rows] = await pool.query("SELECT * FROM users");
+    res.json(rows);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/ping", async (req, res) => {
-  const [result] = await pool.query('SELECT "hello world" as RESULT');
-  res.json(result[0]);
+  try {
+    const [result] = await pool.query('SELECT "hello world" as RESULT');
+    res.json(result[0]);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/create", async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("Mateo")');
-  res.json(result);
+  try {
+    const result = await pool.query('INSERT INTO users(name) VALUES ("Mateo")');
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
--- Backend/src/config.js
+++ Backend/src/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 3000
 
 export const DB_HOST = process.env.DB_HOST
 export const DB_USER = process.env.DB_USER
